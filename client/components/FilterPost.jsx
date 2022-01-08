809c451fe740c503951a782d4dd50e9156b22218
APanina <iriska_panina@mail.ru>
2022-01-08 17:16:09 +0300

    add_clear_input_filter

--- client/components/FilterPost.jsx
+++ client/components/FilterPost.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import MyButton from './UI_components/button/MyButton'
 
 import MyInput from './UI_components/input/MyInput'
 import MySelect from './UI_components/select/MySelect'
 
 const FilterPost = ({ filter, setFilter, optionsSort }) => {
   const getSearchChangeInput = (e) => setFilter({ ...filter, search: e.target.value.toLowerCase() })
+  const getSearchClearInput = () => {
+    return setFilter({ ...filter, search: '' })
+  }
 
   return (
     <div>
       <MyInput placeholder="search..." value={filter.search} onChange={getSearchChangeInput} />
+      <MyButton id="clear_search" onClick={getSearchClearInput}>clear search</MyButton>
       <MySelect
         defaultSort="Sorting by..."
         options={optionsSort}
